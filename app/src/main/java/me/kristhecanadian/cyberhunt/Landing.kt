9c721cf2899fd4dbdeac28eadeac98e9de371d67
Kristopher Beauchemin <kristhecanadian101@gmail.com>
2023-01-21 13:20:03 -0500

    Init Project Template

---
+++ app/src/main/java/me/kristhecanadian/cyberhunt/Landing.kt
@@ -0,0 +1,21 @@
+package me.kristhecanadian.cyberhunt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.kristhecanadian.cyberhunt.R
+import java.util.*
+
+
+class Landing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(this@Landing, Dashboard::class.java))
+                finish()
+            }
+        }, 1000)
+    }
+}
---
+++ app/src/main/java/me/kristhecanadian/cyberhunt/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package me.kristhecanadian.cyberhunt.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Lets Go Explore!"
+    }
+    val text: LiveData<String> = _text
+}
